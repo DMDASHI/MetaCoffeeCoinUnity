add9b575de76e9be017914513b169e421e2c538b
DMDASHI <miguela.giraldoc@uqvirtual.edu.co>
2022-05-28 20:43:46 -0500

    Ordenadas algunas carpetas, añadidos los test

---
+++ Assets/Scripts/FarmScripts/PlayModePlantTests/PlayModePlantTestScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class PlayModePlantTestScript
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void PlayModePlantTestScriptSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+
+}
